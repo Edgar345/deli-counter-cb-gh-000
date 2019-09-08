d6615d10c36315f1cbd96a5b087e5818a3fc27f3
Edgar V <evenegas9@ucmerced.edu>
2019-09-08 03:33:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@
 #"The line is currently: 1. Ada 2. Grace 3. Kent"
 def line array
   if array.empty?
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else
     print "The line is currently:"
     array.each_with_index do |name, index|
