ea7b78d03cee0b59165612549418ad72ede55c0e
Edgar V <evenegas9@ucmerced.edu>
2019-09-08 03:36:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,16 @@
 # Write your code here.
 
 #"The line is currently: 1. Ada 2. Grace 3. Kent"
-def line array
-  if array.empty?
+def line people
+  if people.empty?
     puts "The line is currently empty."
   else
     message = "The line is currently:"
-    array.each_with_index do |name, index|
+    people.each_with_index do |name, index|
       message += " #{index + 1}. #{name}"
     end
     puts message
   end
 end
+
+def take_a_number
